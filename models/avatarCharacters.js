e7e5c741c6eddc6973d8b736a0a558dc43d30ef4
Jessie Zollinger <jzolly@hotmail.com>
2022-05-20 10:12:49 -0400

    file structure setup

---
+++ models/avatarCharacters.js
@@ -0,0 +1,25 @@
+// Require Statements
+const express = require('express');
+
+// Initialize express()
+
+// Configure
+
+// MiddleWare
+
+// Routes INDUCES
+// I
+
+// N
+
+// D
+
+// U
+
+// C
+
+// E
+
+// S
+
+// LISTENERS
